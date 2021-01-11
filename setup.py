21fbe24241a90a85d300cf1d5cf54918cf1dd569
nathan <natebanion@gmail.com>
2021-01-10 23:15:24 -0500

    Update package dependencies.

--- setup.py
+++ setup.py
@@ -7,5 +7,9 @@ setuptools.setup(
     url="https://github.com/DataKind-DC/ifes-elections",
     author="DataKind",
     packages=["src"],
+    install_requires=[
+        "python-dotenv>=0.15.0",
+        "requests>=2.25.1",
+    ],
     zip_safe=False,
 )
