ac370a1a1235c368c3c3e7651d9e29ee7c7e62d4
nathan <natebanion@gmail.com>
2021-01-10 22:32:45 -0500

    Set up basic package infrastructure.

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="ifes-elections",
+    version="0.0.0",
+    description="IFES Elections Data Visualization",
+    url="https://github.com/DataKind-DC/ifes-elections",
+    author="DataKind",
+    packages=["src"],
+    zip_safe=False,
+)
