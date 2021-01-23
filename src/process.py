787f67a428c86b607d50f1f58cd0482b1278748e
nathan <natebanion@gmail.com>
2021-01-23 12:51:23 -0500

    Add src.process.tidy_districts to extract raw district JSON into a dataframe.

--- src/process.py
+++ src/process.py
@@ -35,3 +35,22 @@ def tidy_names(results):
     # Make an election name series.
     return (pd.Series(records, name="election_name")
               .rename_axis(index="election_id"))
+
+
+def tidy_districts(results):
+    """Extract district information from elections json.
+
+    Args:
+        results (list): Results from elections json.
+
+    Returns:
+        pandas.DataFrame: election district information, indexed by election ID.
+    """
+    # Extract district information.
+    records = dict()
+    for result in results:
+        records[result["election_id"]] = result["district"]
+
+    # Make a districts dataframe.
+    return (pd.DataFrame.from_dict(records, orient="index")
+              .rename_axis("election_id"))
--- src/tests/test_process.py
+++ src/tests/test_process.py
@@ -11,10 +11,22 @@ def results():
         {
             "election_id": "0",
             "election_name": {"en_US": "Election 0"},
+            "district": {
+                "district_name": "dn0",
+                "district_type": "dt0",
+                "district_ocd_id": "do0",
+                "district_country": "dc0",
+            },
         },
         {
             "election_id": "1",
             "election_name": {"en_US": "Election 1"},
+            "district": {
+                "district_name": "dn1",
+                "district_type": "dt1",
+                "district_ocd_id": "do1",
+                "district_country": "dc1",
+            },
         },
     ]
 
@@ -33,3 +45,16 @@ def test_tidy_names(results):
     expected = pd.Series(values, index=index, name="election_name")
     result = process.tidy_names(results)
     pdt.assert_series_equal(result, expected)
+
+
+def test_tidy_districts(results):
+    data = {
+        "district_name": ["dn0", "dn1"],
+        "district_type": ["dt0", "dt1"],
+        "district_ocd_id": ["do0", "do1"],
+        "district_country": ["dc0", "dc1"],
+    }
+    index = pd.Index(["0", "1"], name="election_id")
+    expected = pd.DataFrame(data, index)
+    result = process.tidy_districts(results)
+    pdt.assert_frame_equal(result, expected)
