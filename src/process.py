b4609f6c04e0564c58884071e34dedc9063fb78d
nathan <natebanion@gmail.com>
2021-01-23 12:39:36 -0500

    Add src.process.tidy_names to extract raw name data into a series.

--- src/process.py
+++ src/process.py
@@ -1,5 +1,6 @@
 """Utilities for processing raw json exports.
 """
+import pandas as pd
 
 
 def extract_results(raw):
@@ -15,3 +16,22 @@ def extract_results(raw):
     for page in raw:
         results.extend(page["results"])
     return results
+
+
+def tidy_names(results):
+    """Extract names from elections json.
+
+    Args:
+        results (list): Results from elections json.
+
+    Returns:
+        pandas.Series: election names, indexed by election ID.
+    """
+    # Extract election names.
+    records = dict()
+    for result in results:
+        records[result["election_id"]] = result["election_name"]["en_US"]
+
+    # Make an election name series.
+    return (pd.Series(records, name="election_name")
+              .rename_axis(index="election_id"))
--- src/tests/test_process.py
+++ src/tests/test_process.py
@@ -1,5 +1,22 @@
+import pandas as pd
+import pandas.testing as pdt
+import pytest
 from src import process
 
+
+@pytest.fixture
+def results():
+    """Example election records from raw JSON."""
+    return [
+        {
+            "election_id": "0",
+            "election_name": {"en_US": "Election 0"},
+        },
+        {
+            "election_id": "1",
+            "election_name": {"en_US": "Election 1"},
+        },
+    ]
 
 def test_extract_results():
     raw = [
@@ -8,3 +25,11 @@ def test_extract_results():
     ]
     expected = ["r00", "r01", "r10", "r11"]
     assert process.extract_results(raw) == expected
+
+
+def test_tidy_names(results):
+    values = ["Election 0", "Election 1"]
+    index = pd.Index(["0", "1"], name="election_id")
+    expected = pd.Series(values, index=index, name="election_name")
+    result = process.tidy_names(results)
+    pdt.assert_series_equal(result, expected)
